e80655a79da9c2dc12d5666f1dab7b97495a46ce
Ronney Arias <ronneyarias@gmail.com>
2023-10-17 19:59:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  function superbowlWin(record) {
+    let winningYear = record.find(function(record) {
+      return record.result === "W";
+    });
+   
+    if (winningYear) {
+        return winningYear.year;
+      } else {
+        return undefined;
+      }
+    }
